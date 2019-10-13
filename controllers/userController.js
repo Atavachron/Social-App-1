c9ea3c219cf7fca3a4af5cc315db7aa20d82e73f
Atavachron <vladimir.kalichuk@gmail.com>
2019-10-13 15:29:15 +0400

    Added controllers

---
+++ controllers/userController.js
@@ -0,0 +1,16 @@
+exports.login = function() {
+
+}
+
+exports.logout = function() {
+  
+}
+
+exports.register = function(req, res) {
+  res.send("Thanks for registering")
+}
+
+exports.home  = function(req, res) {
+  res.render('home-guest');
+}
+
--- router.js
+++ router.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const userController = require('./controllers/userController')
 
-router.get('/', (req, res) => {
-  res.render('home-guest')
-})
+router.get('/', userController.home);
+router.post('/register', userController.register)
 
-module.exports = router; 
+module.exports = router;
+
+
+
+
+// const express = require('express');
+// const router = express.Router();
+
+// router.get('/', (req, res) => {
+//   res.render('home-guest')
+// })
+
+// module.exports = router; 
